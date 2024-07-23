4647dad020facb88a496f4e49301f5c9972b1048
trnguyenuka <trnguyenuka@github.com>
2024-07-23 14:16:41 +0200

    update

--- run_GCTree_pipeline_separate_conditions.sh
+++ run_GCTree_pipeline_separate_conditions.sh
@@ -5,18 +5,19 @@ project_src="/home/hieunguyen/CRC1382/src_2023/bcrtree";
 output=${outdir}/${output_version};
 mkdir -p ${output};
 
-samplesheet="/home/hieunguyen/CRC1382/src_2023/bcrtree/SampleSheets/m11_all_YFP_MIDs.csv"
+samplesheet="/home/hieunguyen/CRC1382/src_2023/bcrtree/SampleSheets/m14_all_YFP_SampleSheet.csv"
 
 deduplicate_src=${project_src}/deduplicated.py;
 modify_tree_colors=${project_src}/modify_tree_colors.py;
 color_path=${project_src}/hex_color.csv;
 
-work=${output}/work;
+work=${outdir}/gctree_pipeline_work;
+
+rm -rf $work
 
 nextflow run GCtree_pipeline_input_SampleSheet.nf \
 --samplesheet ${samplesheet} \
 --output ${output}/${samplesheet%.csv*} \
 --deduplicate_src ${deduplicate_src} \
 --modify_tree_colors ${modify_tree_colors} \
---color_path ${color_path} 
--resume -w ${work}
+--color_path ${color_path} -resume -w ${work}
