d1c25aa134fe2ebae90856acd63942608c43d4d0
trnguyenuka <trnguyenuka@github.com>
2024-08-29 14:35:56 +0200

    check

--- run_docker_gctree_CRC1382_server.sh
+++ run_docker_gctree_CRC1382_server.sh
@@ -2,6 +2,6 @@
 port=$1;
 name=$2;
 password=$3
-sudo docker run -it --cpus 20 --memory 64GB \
+sudo docker run -it --cpus 12 --memory 64GB \
 -v /home/hieunguyen:/home/hieunguyen -v /media:/media -v /mnt:/mnt -p ${port}:8787 -e PASSWORD=${password} --name ${name} -e USERID=1004 -e GROUPID=1005 \
 tronghieunguyen/gctree:latest bash
