dbaffc1678befa4073fea304476b571b92963dec
hieunguyen4193 <work@hieunguyen.de>
2024-09-12 13:37:49 +0200

    update

--- run_mixcr_pipeline.sh
+++ run_mixcr_pipeline.sh
@@ -1,7 +1,7 @@
 ##### batch 1
-# path_to_input="/mnt/storage/data/local/mol_med/bcr/220701_etc_biopsies/samples";
+path_to_input="/mnt/storage/data/local/mol_med/bcr/220701_etc_biopsies/samples";
 ##### batch 2:
-path_to_input="/home/hieu/storage/240826_Simons_pabst_MolMedcine_AmpliSeq/Fastq";
+# path_to_input="/home/hieu/storage/240826_Simons_pabst_MolMedcine_AmpliSeq/Fastq";
 
 output_version=20240903
 outputdir=/home/hieu/outdir/bcrtree_outdir/${output_version};
@@ -12,7 +12,7 @@ for file in $files;do \
 filename=$(echo $file | xargs -n 1 basename);
 sampleid=${filename%_S*};
 fastq1=${file}
-fastq2=${file%R1*}R2_001.fastq.gz; # add .gz depending on the input fastq format ext
+fastq2=${file%R1*}R2_001.fastq; # add .gz depending on the input fastq format ext
 echo -e "------------------------------------------"
 echo -e "WORKING ON SAMPLE " $sampleid "\n"
 echo -e "------------------------------------------"
